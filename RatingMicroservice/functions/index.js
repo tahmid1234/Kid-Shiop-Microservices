84c92dfc1e8b35aa70492e4739cee8bbd83a425b
Tahmid <tahmidislam1998@gmail.com>
2020-12-26 08:28:33 +0600

    RatingMicro service created

--- RatingMicroservice/functions/index.js
+++ RatingMicroservice/functions/index.js
@@ -20,13 +20,7 @@ const db=admin.firestore();
 const cors= require('cors');
 app.use( cors({origin:true}))
 
-const loadPosts = async () => {
-    
-    const response = await getPostss();
-    if (response.ok) {
-      console.log("Yes hoise")
-    }
-  };
+
 //routes
 app.get('/hello-world', (req,res)=>{
     return res.status(200).send('hello');
@@ -34,26 +28,54 @@ app.get('/hello-world', (req,res)=>{
 
 //create
 
-app.post('/hello-world', (req,res)=>{
+app.post('/rate', (req,res)=>{
     (async ()=> {
         try{
-            await db.collection('products').doc('/'+req.body.id+'/')
+            rating= await get_rating(req)
+            
+            //adding new rating section
+            if(rating.rating<1){
+            await db.collection('rating').doc('/'+req.body.productId+'/').collection('rated_product').doc('/'+req.body.raterId+'/')
             .create({
-                name:req.body.name,
-                desc:req.body.description
+                productId:req.body.productId,
+                rating:1,
+                raterId:req.body.raterId,
+               
             })
-            axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(result=>{
-                console.log(result)
+        }
+        else{
+          
+            //updating existing rating
+            update_rating(req,rating.rating+1);
+        }
+
+        //updating product section
+
+        rating_details=await get_rating_details(req)
+        console.log(rating_details," Rating")
+        if(rating_details.totalRating>4)
+        {
+            let averageRating=rating_details.totalRating/rating_details.raters
+            axios.put('http://localhost:8080/microserviceproduc/us-central1/app/product/update/ratingDetails/'+req.body.productId,{
+                    averageRating:averageRating,
+                    raters:rating_details.raters
             })
-            .catch(error=>{
-                console.log(error)
+            .then(result=>
+                {
+                console.log(result);
             })
+            .catch(error=>
+                {
+                console.log(error);
+            });
+        }
+
 
             return res.status(200).send();
 
         }
-        catch (error){
+        catch (error)
+        {
             console.log(error);
             return res.status(500).send();
         }
@@ -62,64 +84,113 @@ app.post('/hello-world', (req,res)=>{
 });
 
 //read
-app.get('/hello-world/read/:id', (req,res)=>{
-    (async ()=> {
-        try{
-            const document=db.collection('products').doc(req.params.id);
-            let product =await document.get();
-            let reponse=product.data();
+//to check how many ratings a user has given to a product
+const get_rating= (async (req,rating)=> 
+{
+        try
+        { 
 
-
-            return res.status(200).send(reponse);
+            const document=db.collection('rating').doc('/'+req.body.productId+'/').collection('rated_product').doc('/'+req.body.raterId+'/');
+            let product =await document.get().then(doc=>
+                {
+                if(!doc.exists)
+                {
+                   
+                    rating={rating:0}
+                }
+                else
+                {
+                console.log(doc.data().rating)
+                rating={rating:doc.data().rating}
+              
+                }
+                
+            }).catch(error=>
+                {
+                console.log("error"+error)
+                return rating={rating:-1};
+            })
+             return rating;
 
         }
         catch (error){
             console.log("Hello")
             console.log(error);
-            return res.status(500).send(error);
+            return  rating={rating:-1};;
         }
 
-    })();
-});
+    });
 
-app.get('/hello-world/read', (req,res)=>{
-    (async ()=> {
-        try{
-            let query=db.collection('products');
+
+    // update existing rating value
+    const update_rating= (async (req,rating,res)=> 
+    {
+        try
+        {            
+            const document=db.collection('rating').doc('/'+req.body.productId+'/').collection('rated_product').doc('/'+req.body.raterId+'/');
+            await document.update
+            ({
         
-            let respons=[];
+                rating:rating,    
+               
+         });
+           
+
+        }
+        catch (error)
+        {
+            console.log("Hello")
+            console.log(error);
+           
+        }
+
+    });
+
+
+
+const get_rating_details=  (async (req)=> 
+{
+        try
+        {
+            let query=db.collection('rating').doc('/'+req.body.productId+'/').collection('rated_product');
+            let rater_map={};
+            let rater=0;
+            let total_rating=0;
             await query.get().then(querySnapshot =>{
                 let docs=querySnapshot.docs;
-                for(let doc of docs){
-                    const selectedItem={
-                        id:doc.id,
-                        name:doc.data().name,
-                        desc:doc.data().desc
+                
+                for(let doc of docs)
+                {
+                    if(!( doc.id in rater_map))
+                    {
+                        rater=rater+1;
+                        rater_map[doc.id]=1
                     }
-                    respons.push(selectedItem)
-
+                    total_rating=total_rating+doc.data().rating;   
                 }
-                return respons;
+                
+                res=
+                {
+                    raters:rater,
+                    totalRating:total_rating
+                }
+                return res;
             })
-
             
-            return res.status(200).send(respons);
+            return res;
 
         }
         catch (error){
             console.log("Hello")
-            console.log(error);
-            return res.status(500).send(error);
+            console.log(error);           
         }
 
-    })();
-});
+    });
 
-//update
 
-//delete
 
 //export the api to firebase cloud functuin
+
 
 exports.app =functions.https.onRequest(app);
 
